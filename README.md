a63cea365497fdfd850c4da2b14178ce26c64427
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-07-01 18:04:44 +0400

    Initial commit

--- README.md
+++ README.md
@@ -2,12 +2,14 @@ goldsky login
 goldsky pipeline apply
 goldsky pipeline start xxx
 
+goldsky から neon に sync する
 https://app.goldsky.com/project_clqequrmv3a5301zk2pzn0az7/data-sources/dataset/raw_logs
 
+neondb で sql を確認する
 https://console.neon.tech/app/projects/jolly-breeze-08921703/query
 
-https://vercel.com/integrations/neon
-
-https://docs.goldsky.com/reference/mirror-functions/decoding-functions
-
+vercel serverless で HTTP 経由でドライバーを使用する
 https://neon.tech/docs/serverless/serverless-driver#use-the-driver-over-http
+
+vercel のプロジェクトに追加する
+https://vercel.com/integrations/neon
--- api/signal.ts
+++ api/signal.ts
@@ -1,9 +1,12 @@
 import { neon } from '@neondatabase/serverless';
-import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(request: NextApiRequest, res: NextApiResponse) {
+export default async (req: Request) => {
   const sql = neon(process.env.DATABASE_URL!);
   // const posts = await sql('SELECT * FROM posts WHERE id = $1', [postId]);
   const record = await sql('SELECT * FROM optimism_sepolia_cred_events');
-  return res.status(200).send(record);
-}
+  return new Response(JSON.stringify(record));
+};
+
+export const config = {
+  runtime: 'edge',
+};
