f67d1a1e8d11752670dc09128f0de30de276ed00
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-07-02 00:46:40 +0400

    feat: update

--- api/signal.ts
+++ api/signal.ts
@@ -27,7 +27,7 @@ export default async (req: Request) => {
     WHERE
       rn = 1
     ORDER BY
-      latest_supply DESC::numeric;
+      latest_supply DESC;
   `;
 
   const record = await sql(query);
